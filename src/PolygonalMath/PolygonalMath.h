16cf055c451135afd20b0ca3636c18184b0661ae
ChrisUbben <ubben.chris@gmail.com>
2018-09-01 16:59:54 -0700

    Not working

---
+++ src/PolygonalMath/PolygonalMath.h
@@ -0,0 +1,14 @@
+//-----------------------------------------------------------------------------
+// Filename: PolygonalMath.h
+// Project: PolygonalMath
+// Author: Chris Ubben
+// Creation Date: 2018/09/01
+//-----------------------------------------------------------------------------
+
+#ifndef POLYGONALMATH_POLYGONALMATH_H
+#define POLYGONALMATH_POLYGONALMATH_H
+
+// Vector
+#include "Vec2.h"
+
+#endif
---
+++ src/PolygonalMath/Vector/Vec2.cpp
@@ -0,0 +1,21 @@
+//-----------------------------------------------------------------------------
+// Filename: Vec2.cpp
+// Project: PolygonalMath
+// Author: Chris Ubben
+// Creation Date: 2018/09/01
+//-----------------------------------------------------------------------------
+
+// Precompiled Headers
+#include "stdafx.h"
+
+// System Headers
+
+// Library Headers
+
+// Project Headers
+#include "Vec2.h"
+#include <iostream>
+
+namespace PolygonalMath
+{
+}
