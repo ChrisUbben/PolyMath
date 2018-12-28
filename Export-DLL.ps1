f369368f0e11e72b9721d1301be97375f3b815db
ChrisUbben <ubben.chris@gmail.com>
2018-12-27 18:09:47 -0800

    Moved build event to ps1 and cleaned up project

---
+++ Export-DLL.ps1
@@ -0,0 +1,33 @@
+<#
+.SYNOPSIS
+Recursively gathers all header files and makes them available in the output directory.
+
+.DESCRIPTION
+Recursively gathers all header files and makes them available in the output directory.
+To use this script, add the following to the post build events in VS:
+powershell.exe -NoProfile -Command "& '$(SolutionDir)Export-DLL.ps1' -ProjectDir '$(ProjectDir)' -OutputPath '$(OutputPath)'"
+
+TODO: Add support to generate a master <Project-Name>.h, package everything up as zip, and not copy excluded files (stdfx.h, targetver.h, etc.).
+
+.PARAMETER ProjectDir
+The $(ProjectDir) macro to be filled in by VS.
+
+.PARAMETER OutputPath
+The $(OutputPath) macro to be filled in by VS.
+#>
+param(
+    [Parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [string]$ProjectDir,
+
+    [Parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [string]$OutputPath
+)
+
+Get-ChildItem "$ProjectDir*.h" -Recurse | Copy-Item -Destination "$OutputPath"
+
+# # Collect all source files
+# $SourceFiles = Get-ChildItem -Recurse | Where-Object {$_.Name -like "*.cpp" -or $_.Name -like "*.h"}
+# powershell -Command "#Get-Content '$(ProjectDir)PolygonalMath.h' | ForEach-Object { if ($_ -match '"(?<HeaderName>.*.h)"') { $Matches['HeaderName'] } }'"
+# powershell -Command "#Remove-Item -Path '$(OutputPath)target.h'"
